9a76fef03e5f289a1cb5004dab80abe71634881c
Stryfura <shilunett@gmail.com>
2024-09-18 18:05:34 +0900

    Added new models. Added code of plain sql execution, transactions and joins

--- src/main/scala/mypackage/Main.scala
+++ src/main/scala/mypackage/Main.scala
@@ -1,5 +1,7 @@
 package mypackage
 
+import slick.jdbc.GetResult
+
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
@@ -15,6 +17,12 @@ object Main {
 
   val quattroFormaggi = Pizza(1L, "Quattro Formaggi", 10, 4)
 
+  val gorgonzola = Ingredient(1L, "Gorgonzola")
+  val mozzarella = Ingredient(2L, "Mozzarella")
+  val brie = Ingredient(3L, "Brie")
+  val feta = Ingredient(4L, "Feta")
+  val salmon = Ingredient(4L, "Salmon")
+
   def insertPizza(): Unit = {
     val queryDescription = SlickTables.pizzaTable += quattroFormaggi
     val futureId: Future[Int] = Connection.db.run(queryDescription)
@@ -24,7 +32,8 @@ object Main {
       case Failure(e) => println(s"Execution failed: $e")
     }
     //Apparently the executor finishes before applying the statement, since it is a test a sleep should do
-    Thread.sleep(200)
+    //Refactored to the main function
+    //Thread.sleep(200)
   }
 
   def readAllPizzas(): Unit = {
@@ -33,7 +42,6 @@ object Main {
       case Success(pizzas) => println(s"Fetched: ${pizzas.mkString(", ")}")
       case Failure(e) => println(s"Error: $e")
     }
-    Thread.sleep(200)
   }
 
   def readSomePizzas(): Unit = {
@@ -42,7 +50,6 @@ object Main {
       case Success(pizzas) => println(s"Fetched: ${pizzas.mkString(", ")}")
       case Failure(e) => println(s"Error: $e")
     }
-    Thread.sleep(200)
   }
 
   def updatePizza(): Unit = {
@@ -53,12 +60,52 @@ object Main {
       case Success(recordsAffected) => println(s"Success, records affected: $recordsAffected")
       case Failure(e) => println(s"Execution failed: $e")
     }
-    Thread.sleep(200)
   }
 
   def deletePizzas(): Unit = {
     Connection.db.run(SlickTables.pizzaTable.filter(_.name.like("%formaggi%")).delete)
-    Thread.sleep(200)
+  }
+
+  def insertIngredient(): Unit = {
+    val queryDescription = SlickTables.ingredientTable ++= Seq(mozzarella, gorgonzola, feta, brie)
+    val futureId = Connection.db.run(queryDescription)
+
+    futureId.onComplete {
+      case Success(recordsAffected) => println(s"Success, records affected: $recordsAffected")
+      case Failure(e) => println(s"Execution failed: $e")
+    }
+    //Apparently the executor finishes before applying the statement, since it is a test a sleep should do
+    //Refactored to the main function
+    //Thread.sleep(200)
+  }
+
+  def executeQuery(): Future[Vector[Pizza]] = {
+    implicit val getResultPizza: GetResult[Pizza] =
+      GetResult(positionedResult => Pizza(
+        positionedResult.<<,
+        positionedResult.<<,
+        positionedResult.<<,
+        positionedResult.<<))
+    val stmt = sql""" select * from pizzeria."Pizza"""".as[Pizza]
+
+    Connection.db.run(stmt)
+  }
+
+  def executeTransaction(): Unit = {
+    val insertPizza = SlickTables.pizzaTable += quattroFormaggi
+    val insertIngredient = SlickTables.ingredientTable += salmon
+    val queryStmts = DBIO.seq(insertPizza,insertIngredient)
+    Connection.db.run(queryStmts.transactionally)
+  }
+
+  def findIngredientsOfPizza(pizzaId: Long): Future[Seq[Ingredient]] = {
+    val joinStmt = SlickTables.pizzaIngredientTable
+      .filter(_.pizzaId === pizzaId)
+      .join(SlickTables.ingredientTable)
+      .on(_.ingredientId === _.id)
+      .map(_._2)
+
+    Connection.db.run(joinStmt.result)
   }
 
   def main(args: Array[String]): Unit = {
@@ -66,5 +113,19 @@ object Main {
     //readAllPizzas()
     //readSomePizzas()
     //updatePizza()
+    //insertIngredient()
+    //executeTransaction()
+    /*
+    findIngredientsOfPizza(1L).onComplete{
+      case Success(ingredients) => println(s"Success:$ingredients")
+      case Failure(e) => println(s"Exception: $e")
+    }
+    */
+    executeQuery().onComplete{
+      case Success(pizzas) => println(s"Success, pizzas: ${pizzas}")
+      case Failure(e) => println(s"Exception: $e")
+    }
+    Thread.sleep(2000)
+    PrivateExecutionContext.executor.shutdown()
   }
 }
--- src/main/scala/mypackage/Model.scala
+++ src/main/scala/mypackage/Model.scala
@@ -2,10 +2,13 @@ package mypackage
 import slick.lifted.ProvenShape
 
 case class Pizza(id: Long, name: String, price: Int, nIngredients: Int)
+case class Ingredient(id: Long, name: String)
+case class PizzaIngredient(id: Long, pizzaid: Long, ingredientid: Long)
 
 object SlickTables {
   import slick.jdbc.PostgresProfile.api._
 
+  //Entity = Pizza
   class PizzaTable(tag: Tag) extends Table[Pizza](tag, Some("pizzeria"), "Pizza"){
     def id = column[Long]("pizza_id", O.PrimaryKey, O.AutoInc)
     def name = column[String]("name")
@@ -14,6 +17,23 @@ object SlickTables {
 
     override def *  = (id, name, price, nIngredients) <> (Pizza.tupled, Pizza.unapply)
   }
-
   lazy val pizzaTable = TableQuery[PizzaTable]
+
+  //Entity = Ingredient
+  class IngredientTable(tag: Tag) extends Table[Ingredient](tag, Some("pizzeria"), "Ingredient"){
+    def id = column[Long]("ingredient_id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("name")
+
+    override def *  = (id, name) <> (Ingredient.tupled, Ingredient.unapply)
+  }
+  lazy val ingredientTable = TableQuery[IngredientTable]
+
+  class PizzaIngredientTable(tag: Tag) extends Table[PizzaIngredient](tag, Some("pizzeria"), "PizzaIngredientMapping"){
+    def id = column[Long]("pizza_ingredient_id", O.PrimaryKey, O.AutoInc)
+    def pizzaId = column[Long]("pizza_id")
+    def ingredientId = column[Long]("ingredient_id")
+
+    override def *  = (id, pizzaid, ingredientid) <> (PizzaIngredient.tupled, PizzaIngredient.unapply)
+  }
+  lazy val pizzaIngredientTable = TableQuery[PizzaIngredientTable]
 }
